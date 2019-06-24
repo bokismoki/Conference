f7080e6ce0123203b7bfc523388ae0f580b2a695
bokismoki <borisbosnjak084@gmail.com>
2019-06-24 14:44:04 +0200

    v1.2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const videoPopup1 = document.querySelector(".video-popup1");
 const videoPopup2 = document.querySelector(".video-popup2");
 const close1 = document.querySelector(".close1");
 const close2 = document.querySelector(".close2");
+const close3 = document.querySelector(".close3");
 
 heroPlayBtn.addEventListener("click", () => {
 
@@ -30,10 +31,10 @@ close2.addEventListener("click", () => {
 
 });
 
-// BROCHURE, SCROLL-TOP AND NAV POPUP
+// BROCHURE-BTN, SCROLL-TOP AND NAV
 const introduction = document.getElementById("introduction");
-const brochure = document.querySelector(".brochure");
-const scrollTop = document.querySelector(".scroll-top");
+const brochureBtn = document.querySelector(".brochure-btn");
+const scrollTopBtn = document.querySelector(".scroll-top-btn");
 const nav = document.querySelector(".nav");
 
 document.addEventListener("scroll", () => {
@@ -41,14 +42,14 @@ document.addEventListener("scroll", () => {
     let introductionPosition = introduction.getBoundingClientRect();
     if (introductionPosition.y <= 0) {
 
-        brochure.style.left = "-55px";
-        scrollTop.style.bottom = "20px";
+        brochureBtn.style.left = "-55px";
+        scrollTopBtn.style.bottom = "20px";
         setTimeout(navTimeoutF, 500);
 
     } else if (introductionPosition.y > 100) {
 
-        brochure.style.left = "-50vw";
-        scrollTop.style.bottom = "-50vh";
+        brochureBtn.style.left = "-50vw";
+        scrollTopBtn.style.bottom = "-50vh";
         setTimeout(navTimeoutA, 500);
 
     }
@@ -62,3 +63,106 @@ const navTimeoutF = () => {
 const navTimeoutA = () => {
     nav.style.position = "absolute";
 };
+
+// BROCHURE POPUP
+const brochureOverlay = document.querySelector(".brochure-overlay");
+
+brochureBtn.addEventListener("click", () => {
+
+    brochureOverlay.classList.toggle("show");
+
+});
+
+close3.addEventListener("click", () => {
+
+    brochureOverlay.classList.toggle("show");
+
+});
+
+// TOGGLE SPEAKERS X AND SHOW ICONS FOR EACH
+const exes = document.querySelectorAll(".x");
+
+exes.forEach(x => {
+
+    x.addEventListener("click", () => {
+
+        x.classList.toggle("xA");
+        x.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle("iconsA");
+
+    });
+
+});
+
+// CHANGE DAY-BTN CLASSES
+const dayBtns = document.querySelectorAll(".day-btn");
+const dayBtn1 = document.querySelector(".day-btn1");
+const dayBtn2 = document.querySelector(".day-btn2");
+const dayBtn3 = document.querySelector(".day-btn3");
+const schedules = document.querySelector(".schedules");
+
+dayBtns.forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        if (btn.classList.contains("day-btn1")) {
+
+            btn.classList.add("active");
+            dayBtn2.classList.remove("active");
+            dayBtn3.classList.remove("active");
+            schedules.classList.add("toggleOpacity");
+            setTimeout(returnOpacity, 250);
+
+        } else if (btn.classList.contains("day-btn2")) {
+
+            btn.classList.add("active");
+            dayBtn1.classList.remove("active");
+            dayBtn3.classList.remove("active");
+            schedules.classList.add("toggleOpacity");
+            setTimeout(returnOpacity, 250);
+
+        } else if (btn.classList.contains("day-btn3")) {
+
+            btn.classList.add("active");
+            dayBtn1.classList.remove("active");
+            dayBtn2.classList.remove("active");
+            schedules.classList.add("toggleOpacity");
+            setTimeout(returnOpacity, 250);
+
+        }
+
+    });
+
+});
+
+const returnOpacity = () => {
+    schedules.classList.remove("toggleOpacity");
+}
+
+// SHOW FULL SCREEN IMAGES FROM GALLERY SECTION ON CLICK
+const gals = document.querySelectorAll(".gal");
+const galleryImgOverlay = document.querySelector(".gallery-img-overlay");
+const galleryImgOverlayImg = document.querySelector(".gallery-img-overlay img");
+const close4 = document.querySelector(".close4");
+
+gals.forEach(gal => {
+
+    gal.addEventListener("click", () => {
+
+        let styles = window.getComputedStyle(gal, false);
+        let bgImg = styles.backgroundImage;
+        let bgHeight = styles.height;
+        let bgWidth = styles.width;
+        galleryImgOverlay.classList.toggle("show");
+        galleryImgOverlayImg.src = bgImg.slice(4, -1).replace(/"/g, "");
+        galleryImgOverlayImg.style.width = bgWidth;
+        galleryImgOverlayImg.style.height = bgHeight;
+
+    });
+
+});
+
+close4.addEventListener("click", () => {
+
+    galleryImgOverlay.classList.remove("show");
+
+});
